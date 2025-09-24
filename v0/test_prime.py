c0fdec6475c4f07ad0b1ca7a8ea47b171e8e6565
Andrea Jans <57602919+awjans@users.noreply.github.com>
2025-09-23 14:31:31 -1000

    Fix indentation in prime test cases

--- v0/test_prime.py
+++ v0/test_prime.py
@@ -2,23 +2,20 @@ import unittest
 from prime import is_prime 
 
 class PrimesTestCase(unittest.TestCase):
-   def test_is_five_prime(self):    
-      self.assertTrue(is_prime(5)) 
-	   
-   def test_if_foo_prime(self):
-     self.assertFalse(is_prime('foo'))
+	def test_is_five_prime(self):    
+    	self.assertTrue(is_prime(5)) 
+	
+	def test_if_foo_prime(self):
+		self.assertFalse(is_prime('foo'))
 
-   def test_if_two_prime(self):
-	 self.assertFalse(is_prime(2))
+	def test_if_two_prime(self):
+		self.assertFalse(is_prime(2))
 
-   def test_if_one_prime(self):
-     self.assertFalse(is_prime(1))
+	def test_if_one_prime(self):
+		self.assertFalse(is_prime(1))
 
-   def test_of_neg_one_prime(self):
-	 self.assertFalse(is_prime(-1))
-   
-
-   
+	def test_of_neg_one_prime(self):
+		self.assertFalse(is_prime(-1))
 
 if __name__ == '__main__':
 	unittest.main()
